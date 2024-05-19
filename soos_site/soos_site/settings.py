e85eca449956c5854d89cdfb54e3a688bc29742d
Alvis Ng <alvis.ng.business@gmail.com>
2024-05-20 03:10:52 +0800

    Update Allowed Hosts

--- soos_site/soos_site/settings.py
+++ soos_site/soos_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-pms4&35e94s#zqv%t7g@crap=885mp-*+ekf&v+gl)*d9*dgs-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
